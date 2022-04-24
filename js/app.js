05752b2c3ee34955d109cfefa8d4921667e3b47b
mostafa osama <mostafaelshrombably775@gmail.com>
2022-04-24 03:32:39 +0200

    threeupdate

--- js/app.js
+++ js/app.js
@@ -21,3 +21,14 @@ let counter = setInterval(() => {
     }
 
 },1000);
+
+let span = document.querySelector(".Toup");
+window.onscroll = function (){
+    this.scrollY >= 1000 ? span.classList.add("show") : span.classList.remove("show");
+}
+span.onclick = function () {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    })
+}
